c5e1c869bee2dbe945f7c8ab0434b5112e98aee0
Lucas Paz <lucasaraujopaz@hotmail.com>
2022-12-13 03:51:14 -0300

    Arg input opcional

--- src/compilador/L1.java
+++ src/compilador/L1.java
@@ -9,8 +9,7 @@ public class L1 {
 	private static final String EXTENSAO = ".l1";
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		
-		final String instrucoesDeUso = "Uso: \n" + "check <file> \n" + "run <input> <file> \n"; 
+		final String instrucoesDeUso = "Uso: \n" + "check <file> \n" + "run <input?> <file> \n"; 
 		
 		Testes.asseverar(args.length > 0, instrucoesDeUso);
 	    final String comando = args[0];
@@ -23,9 +22,9 @@ public class L1 {
 				Parser.checar(codigoFonte);
 	    	}
 	    	case "run" -> {
-	    		Testes.asseverar(args.length > 2, instrucoesDeUso);
-	    		final String input = args[1];
-	    		final String arquivo = args[2];
+	    		Testes.asseverar(args.length > 1, instrucoesDeUso);
+	    		final String input = args.length == 2 ? "" : args[1];
+	    		final String arquivo = args.length == 2 ? args[1] : args[2];
     			final String codigoFonte = Files.readString(Paths.get(arquivo));
     			System.out.println(Parser.rodar(input, codigoFonte));
 	    	}
